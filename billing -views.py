e3b6ab54b200d0905791d5efc30e0457084b32d3
vennapusavijayamani <vennapusavijayamani@gmail.com>
2026-07-05 13:53:38 +0530

    Delete billing -views.py

--- billing -views.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.bill_list, name="bill_list"),
-    path("add/", views.add_bill, name="add_bill"),
-    path("delete/<int:id>/", views.delete_bill, name="delete_bill"),
-]
