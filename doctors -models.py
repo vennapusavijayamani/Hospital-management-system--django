e4556a28b279f91de0142556099dee2928ad878e
vennapusavijayamani <vennapusavijayamani@gmail.com>
2026-07-05 13:00:12 +0530

    Create doctors -models.py

---
+++ doctors -models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
