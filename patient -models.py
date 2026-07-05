a6a4f376f9d8bb86ea379554da852a4e04b568fb
vennapusavijayamani <vennapusavijayamani@gmail.com>
2026-07-05 13:39:00 +0530

    Create patient -models.py

---
+++ patient -models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    phone = models.CharField(max_length=15)
+    disease = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
